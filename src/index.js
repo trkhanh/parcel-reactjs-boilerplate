dd94e8d92182260153aaa43026e6741e8206d80a
Tran Khanh <tran_khanh@outlook.com>
2021-10-16 11:44:35 +0700

    fea: new struct

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import { render } from 'react-dom'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import './index.css'
+
+const App = () => <>Hello World!</>
+
+render(<App />, document.getElementById('root'))
